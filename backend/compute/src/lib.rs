4fabcf18dbf43ae33c7c64111960274eafc955c5
Shivanshu Mangal <shivanshumangal007@gmail.com>
2026-08-10 15:25:14 +0530

    initial commit

--- backend/compute/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod folder_manager;
-pub mod api;
-pub mod zipops;
-pub mod markdown_generator;
-pub mod markdown_meta;
-pub mod endpoint_writer;
--- backend/webserver/libs/edms/src/ops/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod endpoint_ops;
-pub mod request_ops;
-pub mod response_ops;
-pub mod tag_ops;
